9a8ed7f0d6ea2b1aed2834c56b6155dda142ae94
sao880 <matthew.beasley@capitalone.com>
2018-10-26 09:50:57 -0500

    updated keys

--- travis-utilities/release.sh
+++ travis-utilities/release.sh
@@ -2,7 +2,7 @@
 
 cp travis-utilities/.travis.settings.xml $HOME/.m2/settings.xml
 
-openssl aes-256-cbc -K $encrypted_b1566fc596de_key -iv $encrypted_b1566fc596de_iv -in travis-utilities/keys.gpg.enc -out keys.gpg -d
+openssl aes-256-cbc -K $encrypted_00b218189f3e_key -iv $encrypted_00b218189f3e_iv -in travis-utilities/keys.gpg.enc -out keys.gpg -d
 
 gpg --fast-import keys.gpg
 
