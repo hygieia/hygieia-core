570298e9bcd6e802ba64e55a1925bf11c56f2c7e
Subodh <subodh.battina@gmail.com>
2018-11-19 21:53:26 -0600

    Added options constant for the fix to create feature widget via remoteCreate API

--- src/main/java/com/capitalone/dashboard/util/FeatureCollectorConstants.java
+++ src/main/java/com/capitalone/dashboard/util/FeatureCollectorConstants.java
@@ -11,27 +11,35 @@ package com.capitalone.dashboard.util;
  *
  */
 public final class FeatureCollectorConstants {
-    public static final String JIRA = "Jira";
-    public static final String JIRA_XRAY = "Jira XRay";
+        public static final String JIRA = "Jira";
+        public static final String VERSIONONE = "VersionOne";
+        public static final String GITLAB = "GitlabFeature";
+        @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
+        // not an IP
+        public static final String AGENT_VER = "01.00.00.01";
+        public static final String AGENT_NAME = "Hygieia Dashboard - VersionOne Feature Collector";
+		
+		public static final String STORY_HOURS_ESTIMATE = "hours";
+		public static final String STORY_POINTS_ESTIMATE = "storypoints";
+		public static final String STORY_COUNT_ESTIMATE = "count";
+		
+		public static final String SPRINT_SCRUM = "scrum";
+		public static final String SPRINT_KANBAN = "kanban";
+		
+		public static final String TEAM_ID_ANY = "any";
+		public static final String PROJECT_ID_ANY = "any";
 
-    public static final String VERSIONONE = "VersionOne";
-    public static final String GITLAB = "GitlabFeature";
-    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
-    // not an IP
-    public static final String AGENT_VER = "01.00.00.01";
-    public static final String AGENT_NAME = "Hygieia Dashboard - VersionOne Feature Collector";
+		public static final String TOOL_TYPE = "featureTool";
+        public static final String PROJECT_NAME = "projectName";
+        public static final String PROJECT_ID = "projectId";
+        public static final String TEAM_NAME = "teamName";
+        public static final String TEAM_ID = "teamId";
+        public static final String ESTIMATE_METRIC_TYPE = "estimateMetricType";
+        public static final String SPRINT_TYPE = "sprintType";
+        public static final String LIST_TYPE = "listType";
+        public static final String SHOW_STATUS = "showStatus";
 
-    public static final String STORY_HOURS_ESTIMATE = "hours";
-    public static final String STORY_POINTS_ESTIMATE = "storypoints";
-    public static final String STORY_COUNT_ESTIMATE = "count";
-
-    public static final String SPRINT_SCRUM = "scrum";
-    public static final String SPRINT_KANBAN = "kanban";
-
-    public static final String TEAM_ID_ANY = "any";
-    public static final String PROJECT_ID_ANY = "any";
-
-    private FeatureCollectorConstants() {
-        // This class should not be instantiable
-    }
+        private FeatureCollectorConstants() {
+                // This class should not be instantiable
+        }
 }
