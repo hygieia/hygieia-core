56f79d8026915196839adb6ac7493ca6ca67b956
nireeshT <nireesh.thiruveedula@capitalone.com>
2018-12-17 14:21:16 -0500

    Audit status tracking for allowed users  (#28)
    * SystemId Whitelist - Allowed service accounts - Changed collection name to service_accounts
    
    * Added new audit status - SCM_AUTHOR_WHITELISTED_USER

--- src/main/java/com/capitalone/dashboard/status/CodeReviewAuditStatus.java
+++ src/main/java/com/capitalone/dashboard/status/CodeReviewAuditStatus.java
@@ -44,5 +44,6 @@ public enum CodeReviewAuditStatus {
 
     NO_COMMIT_FOR_DATE_RANGE, //Removew this later when we can remove legacy peer review
     COMMIT_AFTER_PR_MERGE, COLLECTOR_ITEM_ERROR,
-    SCM_AUTHOR_LOGIN_INVALID //passed when SCM AUTHOR is unavailable
-}
+    SCM_AUTHOR_LOGIN_INVALID, //passed when SCM AUTHOR is unavailable
+    SCM_AUTHOR_WHITELISTED_USER
+    }
