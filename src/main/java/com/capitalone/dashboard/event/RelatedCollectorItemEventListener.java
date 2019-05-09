a98a80cb42f599cb303a31baeb54e0e330da6521
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-05-09 15:35:19 -0400

    Replaced equalsIgnoreCase (#74)
    * Artifact Audit changes- Collector extends CollectorWithGenericItem
    
    * OSS enhancement
    
    * Replaces equalIgnoreCase
    
    * Replaces equalIgnoreCase

--- src/main/java/com/capitalone/dashboard/event/RelatedCollectorItemEventListener.java
+++ src/main/java/com/capitalone/dashboard/event/RelatedCollectorItemEventListener.java
@@ -7,11 +7,12 @@ import com.capitalone.dashboard.model.relation.RelatedCollectorItem;
 import com.capitalone.dashboard.repository.CollectorItemRepository;
 import com.capitalone.dashboard.repository.CollectorRepository;
 import com.capitalone.dashboard.repository.PipelineRepository;
-import org.apache.commons.lang3.StringUtils;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+
 
 
 @org.springframework.stereotype.Component
@@ -33,7 +34,7 @@ public class RelatedCollectorItemEventListener extends HygieiaMongoEventListener
     public void onAfterSave(AfterSaveEvent<RelatedCollectorItem> event) {
         RelatedCollectorItem relatedCollectorItem = event.getSource();
         try {
-            syncDashboard.sync(relatedCollectorItem,!StringUtils.equalsAnyIgnoreCase(Reason.ARTIFACT_REASON.getAction(),relatedCollectorItem.getReason()));
+            syncDashboard.sync(relatedCollectorItem,!(Reason.ARTIFACT_REASON.getAction().equalsIgnoreCase(relatedCollectorItem.getReason())));
         } catch (SyncException e) {
             LOG.error("Error processing related collector item. ID = " + relatedCollectorItem.getId() + ". Reason " + e.getMessage());
         }
