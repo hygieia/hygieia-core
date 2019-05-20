86f687e158d8fb1346518c4e0a217aeccad3d736
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-05-20 15:39:24 -0400

    LibraryPolicy - Added new Threatlevel - Informational

--- src/main/java/com/capitalone/dashboard/model/LibraryPolicyThreatLevel.java
+++ src/main/java/com/capitalone/dashboard/model/LibraryPolicyThreatLevel.java
@@ -4,7 +4,7 @@ package com.capitalone.dashboard.model;
  * Enumerates the possible statuses.
  */
 public enum LibraryPolicyThreatLevel {
-    Critical, High, Medium, Low, None, Informational;
+    Critical, High, Medium, Low, None,Informational;
 
     public static LibraryPolicyThreatLevel fromString(String value){
         for(LibraryPolicyThreatLevel threatLevel : values()){
