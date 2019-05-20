ca4ce137c8fea602159432c616ba4dc1be028a20
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-05-20 15:37:47 -0400

    LibraryPolicy - added max age for License

--- src/main/java/com/capitalone/dashboard/model/LibraryPolicyThreatLevel.java
+++ src/main/java/com/capitalone/dashboard/model/LibraryPolicyThreatLevel.java
@@ -4,7 +4,7 @@ package com.capitalone.dashboard.model;
  * Enumerates the possible statuses.
  */
 public enum LibraryPolicyThreatLevel {
-    Critical, High, Medium, Low, None;
+    Critical, High, Medium, Low, None, Informational;
 
     public static LibraryPolicyThreatLevel fromString(String value){
         for(LibraryPolicyThreatLevel threatLevel : values()){
