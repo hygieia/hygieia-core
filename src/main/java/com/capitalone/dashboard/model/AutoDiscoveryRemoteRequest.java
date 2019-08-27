2233317c2945b9e7175449fd54792c0da1b47e9f
Daniel (Yunbing) Huang <yunbing.huang@capitalone.com>
2019-08-27 18:37:05 -0400

    move AutoDiscoveryRemoteRequest.java to core

---
+++ src/main/java/com/capitalone/dashboard/model/AutoDiscoveryRemoteRequest.java
@@ -0,0 +1,10 @@
+package com.capitalone.dashboard.model;
+
+public class AutoDiscoveryRemoteRequest extends AutoDiscovery {
+
+    String autoDiscoveryId;
+
+    public String getAutoDiscoveryId() { return autoDiscoveryId; }
+
+    public void setAutoDiscoveryId(String autoDiscoveryId) { this.autoDiscoveryId = autoDiscoveryId; }
+}
