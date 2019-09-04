6f9d3dd8aa403a075bf716c9d6b1af349ef768ba
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-09-04 08:54:02 -0400

    Related items collector model changes (#110)

---
+++ src/main/java/com/capitalone/dashboard/model/AutoDiscoverCollectorItem.java
@@ -0,0 +1,17 @@
+package com.capitalone.dashboard.model;
+
+public class AutoDiscoverCollectorItem extends CollectorItem {
+
+    private AutoDiscoveryStatusType autoDiscoverStatus;
+
+
+    public AutoDiscoveryStatusType getAutoDiscoverStatus() {
+        return autoDiscoverStatus;
+    }
+
+    public void setAutoDiscoverStatus(AutoDiscoveryStatusType autoDiscoverStatus) {
+        this.autoDiscoverStatus = autoDiscoverStatus;
+    }
+
+
+}
