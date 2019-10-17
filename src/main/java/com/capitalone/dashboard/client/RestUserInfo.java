029c531d360098f250784ed2993a5bfd4a36413d
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-10-17 18:52:03 -0400

    Added token in RestUserInfo (#131)
    * get all metadata with searchIdentifier and value
    
    * Added token

--- src/main/java/com/capitalone/dashboard/client/RestUserInfo.java
+++ src/main/java/com/capitalone/dashboard/client/RestUserInfo.java
@@ -3,10 +3,41 @@ package com.capitalone.dashboard.client;
 public class RestUserInfo {
     private String userId;
     private String passCode;
+    private String token;
 
     public RestUserInfo(String userId, String passCode) {
         this.userId = userId;
         this.passCode = passCode;
+    }
+
+    public RestUserInfo(String userId, String passCode,String token) {
+        this.userId = userId;
+        this.passCode = passCode;
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 
     public String getFormattedString() {
