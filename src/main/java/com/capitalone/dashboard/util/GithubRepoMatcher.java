978b0008960d8c87865793e7b863315b7ba7920f
Vidhyalakshmi Vaikundaperumal (CONT) <vidhya9lakshmi@gmail.com>
2020-07-20 10:43:57 -0700

    Added Github matcher orgName

--- src/main/java/com/capitalone/dashboard/util/GithubRepoMatcher.java
+++ src/main/java/com/capitalone/dashboard/util/GithubRepoMatcher.java
@@ -7,8 +7,8 @@ import java.util.regex.Pattern;
 
 public class GithubRepoMatcher {
 
-    public static boolean match (String url, String regex){
-        boolean match = true;
+    public static boolean repoNameMatcher (String url, String regex){
+        boolean repoNameMatcher = true;
 
         try {
             GitHubParsed gitHubParsed = new GitHubParsed(url);
@@ -22,18 +22,48 @@ public class GithubRepoMatcher {
                 matcher = pattern.matcher(text);
 
                 if (!matcher.find()) {
-                    match = false;
+                    repoNameMatcher = false;
                 }
 
             }else{
                 // If repoName is not alphabet the it should filter on A to M
                 if(!regex.contains("a")){
-                    match = false;
+                    repoNameMatcher = false;
                 }
             }
         }catch (Exception ex){
 
         }
-        return match;
+        return repoNameMatcher;
+    }
+
+    public static boolean orgNameMatcher (String url, String regex){
+        boolean orgNameMatcher = true;
+
+        try {
+            GitHubParsed gitHubParsed = new GitHubParsed(url);
+            String text = gitHubParsed.getOrgName().substring(0,1);
+            String alphaRegex = "[a-zA-Z]";
+            Pattern pattern = Pattern.compile(alphaRegex);
+            Matcher matcher = pattern.matcher(text);
+            // First repoName should be Alpha
+            if(matcher.find()){
+                pattern = Pattern.compile(regex);
+                matcher = pattern.matcher(text);
+
+                if (!matcher.find()) {
+                    orgNameMatcher = false;
+                }
+
+            }else{
+                // If repoName is not alphabet the it should filter on A to M
+                if(!regex.contains("a")){
+                    orgNameMatcher = false;
+                }
+            }
+        }catch (Exception ex){
+
+        }
+        return orgNameMatcher;
     }
 }
--- src/test/java/com/capitalone/dashboard/util/GithubRepoMatcherTest.java
+++ src/test/java/com/capitalone/dashboard/util/GithubRepoMatcherTest.java
@@ -10,31 +10,38 @@ public class GithubRepoMatcherTest {
 
     @Test
     public void test_parse_regex_AtoM(){
-        assertTrue(GithubRepoMatcher.match("https://github.com/test/AtoMtest","[a-mA-M]"));
+        assertTrue(GithubRepoMatcher.repoNameMatcher("https://github.com/test/AtoMtest","[a-mA-M]"));
     }
 
     @Test
     public void test_parse_regex_MtoZ(){
-        assertTrue(GithubRepoMatcher.match("https://github.com/test/MtoZtest","[m-zM-Z]"));
+        assertTrue(GithubRepoMatcher.repoNameMatcher("https://github.com/test/MtoZtest","[m-zM-Z]"));
     }
 
     @Test
     public void test_not_match_regex_AtoM(){
-        assertFalse(GithubRepoMatcher.match("https://github.com/test/StoZtest","[a-mA-M]"));
+        assertFalse(GithubRepoMatcher.repoNameMatcher("https://github.com/test/StoZtest","[a-mA-M]"));
     }
 
     @Test
     public void test_not_match_regex_MtoZ(){
-        assertFalse(GithubRepoMatcher.match("https://github.com/test/AtoMtest","[m-zM-Z]"));
+        assertFalse(GithubRepoMatcher.repoNameMatcher("https://github.com/test/AtoMtest","[m-zM-Z]"));
     }
 
     @Test
     public void test_match_not_alphabet(){
-        assertTrue(GithubRepoMatcher.match("https://github.com/test/1toMtest","[a-mA-M]"));
+        assertTrue(GithubRepoMatcher.repoNameMatcher("https://github.com/test/1toMtest","[a-mA-M]"));
     }
 
     @Test
     public void test_not_match_not_alphabet(){
-        assertFalse(GithubRepoMatcher.match("https://github.com/test/1toMtest","[m-zM-Z]"));
+        assertFalse(GithubRepoMatcher.repoNameMatcher("https://github.com/test/1toMtest","[m-zM-Z]"));
     }
+
+
+    @Test
+    public void test_orgName_parse_regex_MtoZ(){
+        assertTrue(GithubRepoMatcher.orgNameMatcher("https://github.com/test/MtoZtest","[m-zM-Z]"));
+    }
+
 }
