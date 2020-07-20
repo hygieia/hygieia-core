c6c4cf6432a853cf578c97473282b92a3bc0448c
Vidhyalakshmi Vaikundaperumal (CONT) <vidhya9lakshmi@gmail.com>
2020-07-20 05:59:43 -0700

    Added Github matcher

---
+++ src/main/java/com/capitalone/dashboard/util/GithubRepoMatcher.java
@@ -0,0 +1,39 @@
+package com.capitalone.dashboard.util;
+
+import com.capitalone.dashboard.model.webhook.github.GitHubParsed;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GithubRepoMatcher {
+
+    public static boolean match (String url, String regex){
+        boolean match = true;
+
+        try {
+            GitHubParsed gitHubParsed = new GitHubParsed(url);
+            String text = gitHubParsed.getRepoName().substring(0,1);
+            String alapRegex = "[a-zA-Z]";
+            Pattern pattern = Pattern.compile(alapRegex);
+            Matcher matcher = pattern.matcher(text);
+            // First repoName should be Alpha
+            if(matcher.find()){
+                pattern = Pattern.compile(regex);
+                matcher = pattern.matcher(text);
+
+                if (!matcher.find()) {
+                    match = false;
+                }
+
+            }else{
+                // If repoName is not alphabet the it should filter on A to M
+                if(!regex.contains("a")){
+                    match = false;
+                }
+            }
+        }catch (Exception ex){
+
+        }
+        return match;
+    }
+}
---
+++ src/test/java/com/capitalone/dashboard/util/GithubRepoMatcherTest.java
@@ -0,0 +1,40 @@
+package com.capitalone.dashboard.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GithubRepoMatcherTest {
+
+
+    @Test
+    public void test_parse_regex_AtoM(){
+        assertTrue(GithubRepoMatcher.match("https://github.com/test/AtoMtest","[a-mA-M]"));
+    }
+
+    @Test
+    public void test_parse_regex_MtoZ(){
+        assertTrue(GithubRepoMatcher.match("https://github.com/test/MtoZtest","[m-zM-Z]"));
+    }
+
+    @Test
+    public void test_not_match_regex_AtoM(){
+        assertFalse(GithubRepoMatcher.match("https://github.com/test/StoZtest","[a-mA-M]"));
+    }
+
+    @Test
+    public void test_not_match_regex_MtoZ(){
+        assertFalse(GithubRepoMatcher.match("https://github.com/test/AtoMtest","[m-zM-Z]"));
+    }
+
+    @Test
+    public void test_match_not_alphabet(){
+        assertTrue(GithubRepoMatcher.match("https://github.com/test/1toMtest","[a-mA-M]"));
+    }
+
+    @Test
+    public void test_not_match_not_alphabet(){
+        assertFalse(GithubRepoMatcher.match("https://github.com/test/1toMtest","[m-zM-Z]"));
+    }
+}
