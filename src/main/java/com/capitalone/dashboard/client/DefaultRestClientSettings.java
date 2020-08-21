5fe1c5ff9879c914ea7096b4a61cf528d42db8dc
Ragha Vema <ragha.vema@capitalone.com>
2020-08-21 00:39:27 -0400

    documentation fix

--- src/main/java/com/capitalone/dashboard/client/DefaultRestClientSettings.java
+++ src/main/java/com/capitalone/dashboard/client/DefaultRestClientSettings.java
@@ -3,7 +3,7 @@ package com.capitalone.dashboard.client;
 import org.springframework.stereotype.Component;
 
 /**
- * Supplier that returns an instance of RestOperations
+ * Supplier that returns an instance of RestClientSettings to aid autowiring
  */
 @Component
 public class DefaultRestClientSettings implements RestClientSettings{
