c8e9bfd89393d095efcf202da9c7ba529122a993
rvema <ragha.vema@capitalone.com>
2020-08-21 00:30:46 -0400

    Changes for RestClient to make it work

---
+++ src/main/java/com/capitalone/dashboard/client/DefaultRestClientSettings.java
@@ -0,0 +1,11 @@
+package com.capitalone.dashboard.client;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Supplier that returns an instance of RestOperations
+ */
+@Component
+public class DefaultRestClientSettings implements RestClientSettings{
+
+}
