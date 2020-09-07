2574f383cd210db5eabf8d2f0faa5c37b780ac21
nireeshT <nireesh.thiruveedula@capitalone.com>
2020-09-07 16:24:48 -0400

    Whitesource collector : Save history of scans. Library look up

--- src/main/java/com/capitalone/dashboard/model/WhiteSourceChangeRequest.java
+++ src/main/java/com/capitalone/dashboard/model/WhiteSourceChangeRequest.java
@@ -1,6 +1,9 @@
 package com.capitalone.dashboard.model;
 
 
+import java.util.List;
+import java.util.Objects;
+
 public class WhiteSourceChangeRequest extends BaseModel {
 
     private Long startDateTime;
@@ -11,6 +14,14 @@ public class WhiteSourceChangeRequest extends BaseModel {
     private String orgName;
     private String productName;
     private String projectName;
+    private long lastUpdated;
+    private String scopeName;
+    private long changeScopeId;
+    private List<String> beforeChange;
+    private List<String> afterChange;
+    private String operator;
+    private String userEmail;
+    private String orgId;
 
     public Long getStartDateTime() {
         return startDateTime;
@@ -74,5 +85,84 @@ public class WhiteSourceChangeRequest extends BaseModel {
 
     public void setProjectName(String projectName) {
         this.projectName = projectName;
+    }
+
+    public long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getScopeName() {
+        return scopeName;
+    }
+
+    public void setScopeName(String scopeName) {
+        this.scopeName = scopeName;
+    }
+
+    public long getChangeScopeId() {
+        return changeScopeId;
+    }
+
+    public void setChangeScopeId(long changeScopeId) {
+        this.changeScopeId = changeScopeId;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public List<String> getBeforeChange() {
+        return beforeChange;
+    }
+
+    public void setBeforeChange(List<String> beforeChange) {
+        this.beforeChange = beforeChange;
+    }
+
+    public List<String> getAfterChange() {
+        return afterChange;
+    }
+
+    public void setAfterChange(List<String> afterChange) {
+        this.afterChange = afterChange;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WhiteSourceChangeRequest that = (WhiteSourceChangeRequest) o;
+        return Objects.equals(getScopeName(), that.getScopeName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31 * getScopeName().hashCode();
+        return result;
     }
 }
