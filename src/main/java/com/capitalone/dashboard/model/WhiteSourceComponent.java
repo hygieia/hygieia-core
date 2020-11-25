3a7411f2532c40d98b692d161f3cfb96ab77b9fa
nireeshT <nireesh.thiruveedula@capitalone.com>
2020-11-25 15:34:26 -0500

    Changed WS unique fields to orgName and project token (#288)

--- src/main/java/com/capitalone/dashboard/model/WhiteSourceComponent.java
+++ src/main/java/com/capitalone/dashboard/model/WhiteSourceComponent.java
@@ -59,12 +59,12 @@ public class WhiteSourceComponent extends CollectorItem {
         if (o == null || getClass() != o.getClass()) return false;
 
         WhiteSourceComponent that = (WhiteSourceComponent) o;
-        return Objects.equals(getProductName(), that.getProductName()) && Objects.equals(getProjectName(), that.getProjectName()) && Objects.equals(getOrgName(), that.getOrgName());
+        return Objects.equals(getOrgName(), that.getOrgName()) && Objects.equals(getProjectToken(), that.getProjectToken());
     }
 
     @Override
     public int hashCode() {
-        int result = 31 * getProductName().hashCode()+ getProjectName().hashCode()+ getOrgName().hashCode();
+        int result = 31 * getOrgName().hashCode()+ getProjectToken().hashCode();
         return result;
     }
 }
