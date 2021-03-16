a33c3085e8922c31de5c3cb10b1819c9337dbfec
nireeshT <nireesh.thiruveedula@capitalone.com>
2021-03-16 13:37:28 -0400

    api - ignore ping logging

--- src/main/java/com/capitalone/dashboard/util/CommonConstants.java
+++ src/main/java/com/capitalone/dashboard/util/CommonConstants.java
@@ -4,5 +4,4 @@ public class CommonConstants {
 
     public static final String HEADER_CLIENT_CORRELATION_ID = "Client-Correlation-ID";
     public static final String HEADER_API_USER = "apiUser";
-    public static final String HEADER_SOURCE = "source-application";
 }
