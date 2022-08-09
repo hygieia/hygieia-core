0a0a048770e6e6b62fb332ee7a97d5bc9fa12a72
dcanar9 <anthony.canar@hotmail.com>
2022-08-09 12:23:20 -0700

    dummy commit

--- src/main/java/com/capitalone/dashboard/model/WhiteSourceComponent.java
+++ src/main/java/com/capitalone/dashboard/model/WhiteSourceComponent.java
@@ -11,6 +11,7 @@ public class WhiteSourceComponent extends CollectorItem {
     public static final String ORG_NAME = "orgName";
     public static final String LOCAL_CONFIG = "localConfig";
 
+    
     public Boolean getLocalConfig(){return (Boolean) getOptions().get(LOCAL_CONFIG);}
     public void setLocalConfig(Boolean b){getOptions().put(LOCAL_CONFIG, b);}
 
