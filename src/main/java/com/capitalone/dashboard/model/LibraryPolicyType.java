d882859921068a3aabd7b41cb120b3fffc0e998d
Ryan Montgomery <ryan.montgomery@capitalone.com>
2022-08-11 10:36:35 -0400

    Add security_cvss3 to library policy type enum

--- src/main/java/com/capitalone/dashboard/model/LibraryPolicyType.java
+++ src/main/java/com/capitalone/dashboard/model/LibraryPolicyType.java
@@ -4,6 +4,7 @@ package com.capitalone.dashboard.model;
 public enum LibraryPolicyType {
     Security,
     License,
+    Security_CVSS3,
     Other;
 
     public static LibraryPolicyType fromString(String value) {
