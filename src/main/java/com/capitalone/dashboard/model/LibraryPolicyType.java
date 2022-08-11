519f2a257fde00813e037a823d9906e6612463b6
Ryan Montgomery <ryan.montgomery@capitalone.com>
2022-08-11 10:40:40 -0400

    change casing

--- src/main/java/com/capitalone/dashboard/model/LibraryPolicyType.java
+++ src/main/java/com/capitalone/dashboard/model/LibraryPolicyType.java
@@ -4,7 +4,7 @@ package com.capitalone.dashboard.model;
 public enum LibraryPolicyType {
     Security,
     License,
-    Security_CVSS3,
+    Security_cvss3,
     Other;
 
     public static LibraryPolicyType fromString(String value) {
